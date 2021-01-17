944ac7f31a89f1f399dc5c6f3e601d8d5b95e8b3
Magnus <76588711+magnusglide@users.noreply.github.com>
2021-01-17 05:15:31 +0100

    Add files via upload

---
+++ js/script-gallery.js
@@ -0,0 +1,26 @@
+var images = [];
+for (var i = 1; i <= 20; i++) {
+  images[i - 1] = "/images/" + i + ".jpg";
+}
+
+async function addImgContainer() {
+  for (x in images) {
+    var div = document.createElement("div");
+    document.getElementsByClassName("main")[0].appendChild(div);
+    div.className = "image_container";
+  }
+}
+addImgContainer().then(addImg());
+
+// addImgContainer().then((z) => {
+//   addImg();
+// });
+
+function addImg() {
+  for (x in images) {
+    var img = document.createElement("img");
+    img.src = images[x];
+    document.getElementsByClassName("image_container")[x].appendChild(img);
+    console.log(images[x]);
+  }
+}
