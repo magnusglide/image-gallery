3cca7e9b979ba3600a71c7c3d11bf45a86f730e8
Magnus <76588711+magnusglide@users.noreply.github.com>
2021-01-17 10:20:51 +0100

    Add files via upload

--- js/script-gallery.js
+++ js/script-gallery.js
@@ -1,6 +1,6 @@
 var images = [];
 for (var i = 1; i <= 20; i++) {
-  images[i - 1] = "https://magnusglide.github.io/image-gallery/images/" + i + ".jpg";
+  images[i - 1] = "/images/" + i + ".jpg";
 }
 
 async function addImgContainer() {
@@ -20,7 +20,45 @@ function addImg() {
   for (x in images) {
     var img = document.createElement("img");
     img.src = images[x];
+    img.alt = x;
     document.getElementsByClassName("image_container")[x].appendChild(img);
-    console.log(images[x]);
+    img.addEventListener("click", function () {
+      showImg(img);
+    });
+    // console.log(images[x]); //for testing
   }
 }
+
+//Modal
+
+var modal = document.getElementById("modal-id");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+for (let i of document.getElementsByTagName("img")) {
+  i.addEventListener("click", function () {
+    showImg(i);
+  });
+}
+
+function showImg(img) {
+  // console.log(img); //for testing
+  modal.style.display = "block";
+  modalImg.src = img.src;
+  captionText.innerHTML = img.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+document.body.addEventListener("keydown", function (e) {
+  if (e.key == "Escape") {
+    modal.style.display = "none";
+  }
+});
+modal.addEventListener("mousedown", function () {
+  modal.style.display = "none";
+});
